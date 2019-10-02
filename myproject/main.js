b1f5287432fd82795c4b4f91a892281fc0ff0cba
Cellinahrob <40630727+Cellinahrob@users.noreply.github.com>
2019-10-02 08:27:56 -0700

    Add files via upload

---
+++ myproject/main.js
@@ -0,0 +1,14 @@
+// selector(s:"menu").EventListener('click',function(){
+// 	this.classList.toggle(token:'open');
+// 	selector(s:'header').classList.toggle(token:'open')
+// 	selector(s:'overlay').classList.toggle(token:'open')
+// })
+
+
+
+
+
+// function selector(s){
+// 	return document.querySelector(s)
+
+// }
